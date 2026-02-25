9ac3d70b00797ffdc7abed76f68dee84a59e58df
suhakwak <aelpora@gmail.com>
2026-02-26 00:50:07 +0900

    Update widget.c

--- src/widget.c
+++ src/widget.c
@@ -139,7 +139,8 @@ static void indicate_connectivity_internal(void) {
 #endif
     default: // ZMK_TRANSPORT_NONE, neither BLE nor USB connected
 #if IS_ENABLED(CONFIG_ZMK_BLE)
-        if (zmk_endpoint_get_preferred_transport() != ZMK_TRANSPORT_NONE &&
+        // if (zmk_endpoint_get_preferred_transport() != ZMK_TRANSPORT_NONE &&
+        if (zmk_endpoints_selected().transport != 0 &&
             zmk_ble_active_profile_is_open()) {
             LOG_CONN_CENTRAL(profile_index, "open", ADVERTISING);
             blink.color = CONFIG_RGBLED_WIDGET_CONN_COLOR_ADVERTISING;
